3bcc905aa14d3aa84ff58a2d279c50953624c7c0
ghajl <michaelhnn@gmail.com>
2018-06-12 20:45:12 +0300

    Remove jquery, add class for local stock list, replace bootstrap dialog with vex dialog

--- app/dbUtils.js
+++ app/dbUtils.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 const dbUtils = {
-	removeStock: async (symbol, socket) => {
-	  await axios.post('/data', {operation: 'REMOVE', symbol});
-	  socket.emit('update');
-	},
+  removeStock: async (symbol, socket) => {
+    await axios.post('/data', {operation: 'REMOVE', symbol});
+    socket.emit('update');
+  },
 
-	addStock: async (symbol, companyName, socket) => {
-	  await axios.post('/data', {operation: 'ADD', symbol, companyName});
-	  socket.emit('update');
-	},
+  addStock: async (symbol, companyName, socket) => {
+    await axios.post('/data', {operation: 'ADD', symbol, companyName});
+    socket.emit('update');
+  },
 
-	getStocks: async () => {
-	  const docs = await axios.get('/data');
-	  return docs.data.data.reduce((acc, company) => {acc[company.symbol] = company.companyName; return acc }, {})        
-	},
+  getStocks: async () => {
+    const docs = await axios.get('/data');
+    return docs.data.data.reduce((acc, company) => {acc[company.symbol] = company.companyName; return acc }, {})        
+  },
 }
 
 export default dbUtils;
